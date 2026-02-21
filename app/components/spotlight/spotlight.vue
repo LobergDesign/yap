800e8edc7f2f111ddc2e4caf356a0827d61d4ef0
Jean Loberg Hansen <jeanloberg@gmail.com>
2026-02-21 17:24:18 +0100

    feat(ui): implement new image component and update spotlight styles
    Introduce a new UiImage component for standardized image handling.
    Refactor spotlight.vue to utilize the new UiImage component.
    Adjust typography styles for h2, h3, and h4 to align with design specifications.

--- app/components/spotlight/spotlight.vue
+++ app/components/spotlight/spotlight.vue
@@ -1,29 +1,48 @@
 <script lang="ts" setup>
+import type { ImageFragmentFragment } from '~/types/generated/graphql';
 defineProps<{
   title: string;
   subHeader?: string | null | undefined;
   linkHref: string;
-  image: any;
+  image: ImageFragmentFragment;
 }>();
 </script>
 <template>
   <div class="spotlight">
-    <NuxtLink :to="linkHref">
-      <NuxtImg
-        :src="'https://eu-central-1-shared-euc1-02.graphassets.com/AO0xo1wOBRUu1cYgu6oD9z/cmlw8i8p5h7or07w2a7iffj86'"
-        :alt="image.alt"
-        class="spotlight__image"
-      />
+    <NuxtLink :to="`/projects/${linkHref}`">
+      <UiImage :image="image" class="spotlight__image" />
     </NuxtLink>
     <div class="grid-r">
-      <div class="grid-c-12 grid-c-sm-6">
-        <h3>{{ title }}</h3>
+      <div class="grid-c-12 grid-c-sm-2">
+        <h4>({{ title }})</h4>
       </div>
-      <div class="grid-c-12 grid-c-sm-6">
-        <p v-if="subHeader">{{ subHeader }}</p>
+      <div class="grid-c-12 grid-c-sm-4">
+        <p v-if="subHeader" class="h3">{{ subHeader }}</p>
+      </div>
+      <div class="grid-c-12 grid-c-sm-2 spotlight__link">
+        <UiLink text="Go to case" :href="`/projects/${linkHref}`" />
       </div>
     </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.spotlight {
+  .h3 {
+    max-width: 280px;
+    margin-top: 0;
+  }
+  &__image {
+    margin-bottom: clamp(2px, 1vw, 10px);
+  }
+  &__link {
+    text-align: left;
+    @include viewport-medium {
+      text-align: right;
+    }
+    a {
+      display: inline-block;
+    }
+  }
+}
+</style>
